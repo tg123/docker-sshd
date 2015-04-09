238777ac8909324f8bdc77652776ffb2fa6aa621
tgic <farmer1992@gmail.com>
2015-04-09 20:21:13 +0800

    init

---
+++ docker.sshd.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+
+	"github.com/docker/docker/pkg/mflag"
+	"github.com/fsouza/go-dockerclient"
+)
+
+var (
+	logger = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+)
+
+func main() {
+
+	config := struct {
+		ListenAddr  string
+		Port        uint
+		KeyFile     string
+		ShowHelp    bool
+		ShowVersion bool
+		Host        string
+		Cmd         string
+	}{}
+
+	mflag.StringVar(&config.Host, []string{"H", "-host"}, "unix:///var/run/docker.sock", "docker host socket")
+	mflag.StringVar(&config.ListenAddr, []string{"l", "-listen_addr"}, "0.0.0.0", "Listening Address")
+	mflag.UintVar(&config.Port, []string{"p", "-port"}, 2232, "Listening Port")
+	mflag.StringVar(&config.KeyFile, []string{"i", "-server_key"}, "/etc/ssh/ssh_host_rsa_key", "Key file for SSH")
+	mflag.StringVar(&config.Cmd, []string{"c", "-command"}, "/bin/bash", "default exec command")
+	mflag.BoolVar(&config.ShowHelp, []string{"h", "-help"}, false, "Print help and exit")
+	mflag.BoolVar(&config.ShowVersion, []string{"-version"}, false, "Print version and exit")
+
+	mflag.Parse()
+
+	if config.ShowHelp {
+		mflag.PrintDefaults()
+		return
+	}
+
+	client, err := docker.NewClient(config.Host)
+	if err != nil {
+		logger.Fatalln("Cannot connect to docker %v", err)
+	}
+
+	server := &ssh.ServerConfig{
+		PublicKeyCallback: func(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+			// TODO query
+			return nil, nil
+		},
+	}
+
+	privateBytes, err := ioutil.ReadFile(config.KeyFile)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	private, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	server.AddHostKey(private)
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", config.ListenAddr, config.Port))
+	if err != nil {
+		logger.Fatalln("failed to listen for connection: %v", err)
+	}
+	defer listener.Close()
+
+	logger.Printf("Docker.sshd started")
+
+	for {
+		c, err := listener.Accept()
+		if err != nil {
+			logger.Printf("failed to accept connection: %v", err)
+			continue
+		}
+
+		logger.Printf("connection accepted: %v", c.RemoteAddr())
+
+		go func() {
+			sshConn, chans, reqs, err := ssh.NewServerConn(c, server)
+
+			if err != nil {
+				logger.Printf("failed to establish ssh connection: %v", err)
+				return
+			}
+
+			exec, err := client.CreateExec(docker.CreateExecOptions{
+				Container:    sshConn.User(),
+				AttachStdin:  true,
+				AttachStdout: true,
+				AttachStderr: true,
+				Tty:          true,
+				Cmd:          []string{config.Cmd},
+			})
+
+			if err != nil {
+				logger.Printf("failed to create docker exec: %v", err)
+				sshConn.Close()
+				return
+			}
+
+			go handleRequests(reqs)
+			// Accept all channels
+			go handleChannels(client, exec.ID, chans)
+		}()
+	}
+}
+
+func handleRequests(reqs <-chan *ssh.Request) {
+	for req := range reqs {
+		if req.Type == "keepalive@openssh.com" {
+			req.Reply(true, nil)
+			continue
+		}
+		logger.Printf("recieved out-of-band request: %+v", req)
+	}
+}
+
+func handleChannels(client *docker.Client, execID string, chans <-chan ssh.NewChannel) {
+	// Service the incoming Channel channel.
+	for newChannel := range chans {
+		// Since we're handling the execution of a shell, we expect a
+		// channel type of "session". However, there are also: "x11", "direct-tcpip"
+		// and "forwarded-tcpip" channel types.
+		if t := newChannel.ChannelType(); t != "session" {
+			newChannel.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", t))
+			continue
+		}
+
+		// At this point, we have the opportunity to reject the client's
+		// request for another logical connection
+		channel, requests, err := newChannel.Accept()
+		if err != nil {
+			logger.Printf("could not accept channel (%s)", err)
+			continue
+		}
+		// fire up bash for this session
+
+		go func() {
+			err := client.StartExec(execID, docker.StartExecOptions{
+				Detach:       false,
+				OutputStream: channel,
+				ErrorStream:  channel,
+				InputStream:  channel,
+				RawTerminal:  false,
+			})
+
+			exit_status := 0
+
+			if err != nil {
+				exit_status = -1
+			}
+
+			channel.SendRequest("exit-status", false, ssh.Marshal(&struct{ uint32 }{uint32(exit_status)}))
+			channel.Close()
+
+			logger.Printf("session closed")
+		}()
+
+		// Sessions have out-of-band requests such as "shell", "pty-req" and "env"
+		go func(in <-chan *ssh.Request) {
+			for req := range in {
+				ok := false
+				switch req.Type {
+				case "shell":
+					// We don't accept any commands (Payload),
+					// only the default shell.
+					if len(req.Payload) == 0 {
+						ok = true
+					}
+				case "pty-req":
+					// Responding 'ok' here will let the client
+					// know we have a pty ready for input
+					ok = true
+					// Parse body...
+					termLen := req.Payload[3]
+					termEnv := string(req.Payload[4 : termLen+4])
+					h, w := parseDims(req.Payload[termLen+4:])
+					client.ResizeExecTTY(execID, h, w)
+					logger.Printf("pty-req '%s'", termEnv)
+				case "window-change":
+					h, w := parseDims(req.Payload)
+					client.ResizeExecTTY(execID, h, w)
+					logger.Printf("windows-changed %v, %v", h, w)
+					continue //no response
+				}
+				if !ok {
+					logger.Printf("declining %s request...", req.Type)
+				}
+				req.Reply(ok, nil)
+			}
+		}(requests)
+	}
+}
+
+// parseDims extracts terminal dimensions (width x height) from the provided buffer.
+func parseDims(b []byte) (int, int) {
+	w := binary.BigEndian.Uint32(b)
+	h := binary.BigEndian.Uint32(b[4:])
+	return int(h), int(w)
+}
